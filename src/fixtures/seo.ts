702796675a1f3a204eefafe234f187d588367f63
Billy Huang <10646003@ntub.edu.tw>
2024-03-09 14:22:52 +0800

    refactor(seo): rename seo url to site url and move to constants

--- src/fixtures/seo.ts
+++ src/fixtures/seo.ts
@@ -4,8 +4,6 @@ export const seoDescription = "I'm a software engineer from Taiwan.";
 
 export const seoKeywords = 'Billy Huang, software engineer, Taiwan';
 
-export const seoUrl = 'https://billy0402.github.io';
-
 export const seoFavoriteIcon = '/favicon.ico';
 
 export const seoSocialPreviewImage = '/images/avatar.jpg';
