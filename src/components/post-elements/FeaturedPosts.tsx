bd5efadb9ee1dd9e3fb7b12ca8ea31c3e292b272
Billy Huang <10646003@ntub.edu.tw>
2024-03-13 13:52:22 +0800

    feat(featured-posts): add see all posts button

--- src/components/post-elements/FeaturedPosts.tsx
+++ src/components/post-elements/FeaturedPosts.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import PostGrid from '@/components/post-elements/PostGrid';
 import type { Post } from '@/models/post';
 
@@ -9,6 +11,11 @@ const FeaturedPosts = ({ posts }: Props) => {
   return (
     <section className='featured-posts'>
       <h2>Featured Posts</h2>
+      <section className='featured-posts__actions'>
+        <Link className='btn' href='/posts'>
+          &gt;&gt; See all posts
+        </Link>
+      </section>
       <PostGrid posts={posts} />
     </section>
   );
